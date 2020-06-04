9c918058e54115c4478e30b446dd2cafd9f798c8
roosap <roosa.paivansalo@gmail.com>
2020-06-04 22:48:22 +0100

    Change theme to black

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
-        primaryColor: Colors.white,
+        primaryColor: Colors.black,
       ),
       home: RandomWords(),
     );
